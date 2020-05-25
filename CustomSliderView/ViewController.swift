6c5e5a61dc14c1941623003caf6bfdb31b42dd1e
이기완 <kiwan.lee@catenoid.net>
2020-05-25 17:43:12 +0900

    Initial Commit

---
+++ CustomSliderView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSliderView
+//
+//  Created by kiwan on 2020/05/25.
+//  Copyright © 2020 kiwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
